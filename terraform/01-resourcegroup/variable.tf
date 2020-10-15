9d52a3e53100a769964216f31f5be5d6a9651323
iamnogada <seungil.han@gmail.com>
2020-10-15 19:46:23 +0900

    tested for resourcegroup

--- terraform/01-resourcegroup/variable.tf
+++ terraform/01-resourcegroup/variable.tf
@@ -3,6 +3,10 @@ variable subscription_id {
     description=" tenant's subscription id"
 }
 
+variable default_vnet {
+    type =string
+    default=""
+}
 variable location {
     type = string
     default ="koreacentral"
