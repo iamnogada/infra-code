907384ed58dbeea9ecb34bfeff96619f323e94b1
iamnogada <seungil.han@gmail.com>
2020-10-20 16:03:44 +0900

    install bastion's sw and devops vm

--- bastion/install.sh
+++ bastion/install.sh
@@ -47,3 +47,17 @@ sudo apt-get install -y nodejs
 
 echo "install openjdk 11"
 sudo apt-get install -y openjdk-11-jre openjdk-11-jdk
+
+echo "install kubectl"
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+echo "install helm"
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
--- bastion/user.sh
+++ bastion/user.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-while read p; do
-    echo "$p"
+while read u; do
+    echo "$u"
+    sudo userdel $u --remove
+done < $1
+
+
+while read u; do
+    echo "$u"
+    sudo useradd -m $u
+    sudo mkdir /home/$u/.ssh
+    sudo chown -R $u:$u /home/$u/.ssh
+    sudo chmod 700 /home/$u/.ssh
+    sudo cp ~/.ssh/authorized_keys /home/$u/.ssh/
+    sudo chown -R $u:$u /home/$u/.ssh/authorized_keys
 done < $1
