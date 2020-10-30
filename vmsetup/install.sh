23d593339a848847bf826f42319cf4c37cba99b5
iamnogada <seungil.han@gmail.com>
2020-10-30 17:11:46 +0900

    vmsetup added

---
+++ vmsetup/install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo "install docker"
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "install terraform"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update
+sudo apt-get install -y terraform
+
+
+echo "install az cli"
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-get update
+sudo apt-get install -y azure-cli
+
+echo "install nodejs"
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo "install openjdk 11"
+sudo apt-get install -y openjdk-11-jre openjdk-11-jdk
+
+echo "install kubectl"
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+echo "install helm"
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
---
+++ vmsetup/user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while read u; do
+    echo "$u"
+    sudo userdel $u --remove
+done < $1
+
+
+while read u; do
+    echo "$u"
+    sudo useradd -m $u
+    sudo mkdir /home/$u/.ssh
+    sudo chown -R $u:$u /home/$u/.ssh
+    sudo chmod 700 /home/$u/.ssh
+    sudo cp ~/.ssh/authorized_keys /home/$u/.ssh/
+    sudo chown -R $u:$u /home/$u/.ssh/authorized_keys
+    sudo cp ~/.ssh/id_rsa /home/$u/.ssh/
+    sudo chown -R $u:$u /home/$u/.ssh/id_rsa
+done < $1
