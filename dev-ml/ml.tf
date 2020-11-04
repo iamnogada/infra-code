433549d087b4244e737e0b2529c660f94b7fbaba
iamnogada <seungil.han@gmail.com>
2020-11-04 12:44:02 +0900

    remove storage acc

--- dev-ml/ml.tf
+++ dev-ml/ml.tf
@@ -18,64 +18,64 @@ module "ml" {
 }
 
 
-resource "azurerm_storage_account" "file" {
-  name                     = "${module.const.short-name}file01"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_kind             = "BlobStorage"
-  account_replication_type = "GRS"
-  tags                     = module.const.tags
-}
-resource "azurerm_storage_account" "dataset" {
-  name                     = "${module.const.short-name}mldefault"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_kind             = "BlobStorage"
-  account_replication_type = "GRS"
-  tags                     = module.const.tags
-}
-resource "azurerm_storage_account" "vrd-dataset" {
-  name                     = "${module.const.short-name}dataset02"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_kind             = "BlobStorage"
-  account_replication_type = "GRS"
-  tags                     = module.const.tags
-}
+# resource "azurerm_storage_account" "file" {
+#   name                     = "${module.const.short-name}file01"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_kind             = "BlobStorage"
+#   account_replication_type = "GRS"
+#   tags                     = module.const.tags
+# }
+# resource "azurerm_storage_account" "dataset" {
+#   name                     = "${module.const.short-name}mldefault"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_kind             = "BlobStorage"
+#   account_replication_type = "GRS"
+#   tags                     = module.const.tags
+# }
+# resource "azurerm_storage_account" "vrd-dataset" {
+#   name                     = "${module.const.short-name}dataset02"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_kind             = "BlobStorage"
+#   account_replication_type = "GRS"
+#   tags                     = module.const.tags
+# }
 
 
-resource "azurerm_key_vault" "keyvault" {
-  name                = "${module.const.short-name}mlkeyvault"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  tenant_id           = module.const.tenant
-  sku_name            = "premium"
+# resource "azurerm_key_vault" "keyvault" {
+#   name                = "${module.const.short-name}mlkeyvault"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   tenant_id           = module.const.tenant
+#   sku_name            = "premium"
 
-  tags = module.const.tags
-}
+#   tags = module.const.tags
+# }
 
-resource "azurerm_application_insights" "loginsight" {
-  name                = "${module.const.long-name}-ml-loginsight"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  application_type    = "web"
+# resource "azurerm_application_insights" "loginsight" {
+#   name                = "${module.const.long-name}-ml-loginsight"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   application_type    = "web"
 
-  tags = module.const.tags
-}
+#   tags = module.const.tags
+# }
 
-resource "azurerm_machine_learning_workspace" "ml" {
-  name                    = "${module.const.long-name}-ml"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  application_insights_id = azurerm_application_insights.loginsight.id
-  key_vault_id            = azurerm_key_vault.keyvault.id
-  storage_account_id      = azurerm_storage_account.dataset.id
+# resource "azurerm_machine_learning_workspace" "ml" {
+#   name                    = "${module.const.long-name}-ml"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   application_insights_id = azurerm_application_insights.loginsight.id
+#   key_vault_id            = azurerm_key_vault.keyvault.id
+#   storage_account_id      = azurerm_storage_account.dataset.id
 
-  identity {
-    type = "SystemAssigned"
-  }
-  tags = module.const.tags
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   tags = module.const.tags
+# }
